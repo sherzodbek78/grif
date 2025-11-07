fd8fe7f447b4d5e441158e1fc743006a97f0c3a9
sherzodbek78 <sd0441012@gmail.com>
2025-11-07 11:39:53 +0500

    1dars

---
+++ newbot.py
@@ -0,0 +1,36 @@
+#ism = "anvar"
+#shahar ="urganch❤"
+#viloyat = "xorazm"
+#print ("shahrim"+' '+ shahar)
+#print(ism_shahar)
+
+#ism = "sherzod"
+#familya = "xasanov"
+#print(f"salom,mening ismim {familya}. {ism}!")
+
+#print('hello World')
+#print('hello \tWorld')
+#print("hello \nworld")
+
+#ism = "sherzod"
+#familya ="xasanov"
+#ism_sharif = f"{ism} {familya}"
+#print(ism_sharif.upper())
+#ism_sharif = ism_sharif.upper()
+#print(ism_sharif.lower())
+#print(ism_sharif.title())
+#print(ism_sharif.capitalize())
+
+#meva ="   olma  "
+#print(meva)
+#print("men "+ meva.lstrip() +" yaxshi koraman")
+#print("men "+ meva.rstrip() +" yaxshi koraman")
+#print("men "+ meva.strip() +" yaxshi koraman")
+#print("men "+ meva +" yaxshi koraman")
+
+#input
+
+#ism = input("ismingiz nima?")
+#print("salom yaxshi" + ism )
+ism = input("isminingiz nima\n>>>")#ism yozishni yangi qatordan boshalaydi
+print("salom "+ ism.title())
