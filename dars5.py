5ebe256dfd0c88a43dd488d74178763c16b9d588
sherzodbek78 <sd0441012@gmail.com>
2026-04-03 17:32:55 +0500

    dd

---
+++ dars5.py
@@ -0,0 +1,53 @@
+#shahar = "Қўқон"
+#viloyat = 'Фарғона'
+
+#matn = "Men yangi 📱 oldim"
+#print(matn)
+
+#ism = 'Ahmad'
+#print("Mening ismim " + ism)
+
+#ism = "ahad"
+#familiya = 'Qayum'
+#print(ism + ' '+familiya)
+
+#ism = "Ahad"
+#familiya = 'Qayum'
+#ism_sharif = f"{ism} {familiya}"
+#print(ism_sharif)
+
+#ism = "James"
+##familiya = 'Bond'
+#print(f"Salom, mening ismim {familiya}. {ism} {familiya}!")
+
+#print('Hello World!')
+#print('Hello \tWorld!')
+#print('Hello \nWorld!')
+
+#ism = "ahad"
+#familya = "qayum"
+#ism_sharif = f"{ism} {familya}"
+#print(ism_sharif.upper())
+kocha ="bog'bon"
+mahalla = "sog'lom"
+tuman = "yangibozor"
+viloyat ="xorazm"
+
+print(kocha.title() + " ko'chasi, \n" + mahalla.upper() + " mahallasi\n, " + \
+    tuman.lower() + " tumani, ()" + viloyat.capitalize() + " viloyati\n")
+    
+    #print(kocha + " ko'chasi, " + mahalla + " mahallasi, " + \
+       # tuman + " tumani, " + viloyat + " viloyati")
+
+    #print(kocha + " ko'chasi, \n" + mahalla + " mahallasi\n, " + \
+      #  tuman + " tumani, " + viloyat + " viloyati\n")
+kocha ="bog'bon"
+mahalla = "sog'lom"
+tuman = "yangibozor"
+viloyat ="xorazm"
+
+print(f"{kocha}   ko'chasi,   mahalla   mahallasi,  + \
+    tuman   tumani,   viloyat   viloyati")
+
+
+
